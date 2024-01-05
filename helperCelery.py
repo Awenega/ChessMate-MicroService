c60962722259da139a70064dc17743418cf3f927
Andrea <andrea@andrea-ubuntu.home>
2024-01-05 21:26:20 +0100

    Corrections

--- helperCelery.py
+++ helperCelery.py
@@ -14,6 +14,7 @@ celery = Celery(
 @celery.task
 def listen_for_game_changes(room_id, db):
     room_ref = db.collection("Rooms")
+    room = room_ref.document(room_id)
     
     end_game = threading.Event()
     timeout = 1200  # Timeout for the game
@@ -26,7 +27,12 @@ def listen_for_game_changes(room_id, db):
                 if playerTwo != '':
                     end_game.set()
 
-    end_game.wait(timeout=timeout)
-    room = room_ref.document(room_id)
-    room.delete()
+    query_watch = room.on_snapshot(on_snapshot)
+
+    try:
+        end_game.wait(timeout=timeout)
+    finally:
+        query_watch.unsubscribe()
+        room.delete()
+
 
