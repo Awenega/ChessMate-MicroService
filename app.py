dd5e0ab7a54777d7b56d49b4261624740fb7bb5f
Awenega <f.sudoso@gmail.com>
2024-02-13 20:30:09 +0100

    Fix bug on url matches

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ api.add_resource(UserImageResource,'/api/v1/user/avatar/<string:id>/<string:prof
 api.add_resource(DefaultImageResource,'/api/v1/user/avatar/<string:id>')
 api.add_resource(UserScore,'/api/v1/user/score/<string:id>')
 api.add_resource(ParseChessBoard,'/api/v1/parse_chessboard')
-api.add_resource(MatchResource,'/api/v1/matches/', '/api/v1/matches/<string:id>')
+api.add_resource(MatchResource,'/api/v1/matches', '/api/v1/matches/<string:id>')
 
 api.add_resource(OnlineResource, '/api/v1/online', '/api/v1/online/<string:id>',
                  resource_class_kwargs={'db': db})
